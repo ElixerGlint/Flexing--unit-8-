d4d35798735913267be2ecde2af8e981cb719a26
Elixer <everett.rumery@gmail.com>
2025-02-20 13:18:22 -0500

    first commit

---
+++ src/Flex.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Flex {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        ArrayList<Integer> rolls = new ArrayList<>();
+
+        
+    }
+}
